bc3dbccae36f5dd9a904ceba71e21c2eb8cb4464
RubenMFranca <rubenmicael2016@gmail.com>
2025-09-13 19:32:52 -0300

    feat:  Adds CORS to allow requests from other domains

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
+/* eslint-disable @typescript-eslint/no-floating-promises */
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.useGlobalPipes(new ValidationPipe());
+
+  app.enableCors({
+    origin: ['http://localhost:8080'],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
+
+  await app.listen(3000);
 }
 bootstrap();
